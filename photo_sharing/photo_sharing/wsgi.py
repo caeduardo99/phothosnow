38504ab6612a6712d3063c8a8d3a220d8023d5ab
Carlos Soria <carlossoria1999@gmail.com>
2024-11-09 10:00:39 -0500

    change rute

--- photo_sharing/photo_sharing/wsgi.py
+++ photo_sharing/photo_sharing/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo_sharing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo_sharing.photo_sharing.settings')
+
 
 application = get_wsgi_application()
